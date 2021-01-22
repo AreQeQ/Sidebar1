44a648e842574f462f57ef95c8a5d4d535f42b41
Arkadiusz <areqeq@o2.pl>
2021-01-22 11:57:51 +0100

    localhost POST in request tab

--- src/pages/Requests.js
+++ src/pages/Requests.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import {useState} from "react";
 
 function Requests() {
+    const [response, setResponse] = useState([])
+
+    fetch('http://localhost:8080', {
+        method: 'POST',
+        body: JSON.stringify({
+            request: 'foo'
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    })
+        .then(response => response.json())
+        .then(json => setResponse(JSON.stringify(json))
+        )
+
     return (
-      <div className="requests">
-        REQUESTS
-      </div>
-    );
-  }
-  
-  export default Requests;
+        <div className="requests">
+            response: {response}
+        </div>)
+
+}
+
+export default Requests;
