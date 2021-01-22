1b7d9f6b2bec23a59a9454da705d5ea8dc65564d
Arkadiusz <areqeq@o2.pl>
2021-01-22 19:09:24 +0100

    Added POST request

--- src/pages/Requests.js
+++ src/pages/Requests.js
@@ -1,25 +1,53 @@
 import React from 'react';
 import {useState} from "react";
+import './Requests.css'
 
 function Requests() {
+    const [inputIp, setInputIp] = useState("http://localhost:8080")
+    const [connectedIp, setConnectedIp] = useState("")
     const [response, setResponse] = useState([])
 
-    fetch('http://localhost:8080', {
-        method: 'POST',
-        body: JSON.stringify({
-            request: 'foo'
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        }
-    })
-        .then(response => response.json())
-        .then(json => setResponse(JSON.stringify(json))
-        )
+    function connect(ip) {
+        fetch(ip, {
+            method: 'POST',
+            body: JSON.stringify({
+                request: 'foo'
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        })
+            .then(response => response.json())
+            .then(json => setResponse(JSON.stringify(json)))
+            .then(() => setConnectedIp(ip))
+            .catch(e => setResponse("ERROR: " + e))
+    }
+
+    function refresh() {
+        connect(connectedIp)
+    }
 
     return (
         <div className="requests">
-            response: {response}
+
+            <div className="input">
+                IP: <input className="ipInput" type={'text'} defaultValue={inputIp}
+                           onChange={e => setInputIp(e.target.value)}/>
+                <button onClick={() => connect(inputIp)}>Connect</button>
+            </div>
+
+            <div className="connection">
+                Connected to: {connectedIp}
+            </div>
+
+            <div className="refresh">
+                <button onClick={() => refresh}>Refresh</button>
+            </div>
+
+            <div className="response">
+                response: {response}
+            </div>
+
         </div>)
 
 }
